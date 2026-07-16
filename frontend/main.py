2527984dd69b5e40e708e8e95627a55bcf4cb6e3
kouketanabernard4-cmyk <kouketanabernard4@gmail.com>
2026-07-16 17:33:51 +0200

    Mise à jour de ALLOWED_HOSTS pour Railway

--- frontend/main.py
+++ frontend/main.py
@@ -86,4 +86,8 @@ def main(page: ft.Page):
     else:
         go_to_login(page)
 
-ft.app(target=main, view=ft.AppView.WEB_BROWSER, port=8560)
+if __name__ == "__main__":
+    import os
+    # Si Railway fournit un port, on l'utilise. Sinon, on prend 8560 localement.
+    port = int(os.getenv("PORT", 8560))
+    ft.app(target=main, port=port, host="0.0.0.0")
